345790a6c432cab9efebf2784a850a5fe1e832d8
Masik091 <maliastep155@gmail.com>
2024-04-18 23:21:37 +0300

    update

---
+++ Last test/JS-last-test/last-test.js
@@ -0,0 +1,5 @@
+      console.log("123123123", 123123);
+      const button = document.querySelector('.btn-one');
+      const footerId = document.querySelector('#one');
+      const allButtons = document.querySelectorAll('button');
+      const footerIdId = document.getElementById('one');
